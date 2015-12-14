1830ea7ddccd66958297df66f563079e86fc7ffc
Shonix <Peterfut123@gmail.com>
2015-12-14 12:23:21 +0100

    Hello

--- src/GUI/MainGUI.java
+++ src/GUI/MainGUI.java
@@ -2,7 +2,7 @@ package GUI;
 
 public class MainGUI {
 	public static void main(String[] args){
-		//hello hello
+		//hello hello, is it me you're looking for. I can see it in your eyes, i can see it in your smile.
 		
 	}
 }
