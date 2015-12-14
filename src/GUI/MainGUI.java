c0714ec262b0dbf9b1a111a3bbc9c5f994f0cf24
Nikiodoz <niki.tamjidi@gmail.com>
2015-12-14 13:56:20 +0100

    Added set look and feel!

--- src/GUI/MainGUI.java
+++ src/GUI/MainGUI.java
@@ -1,7 +1,17 @@
 package GUI;
 
+import javax.swing.UIManager;
+
 public class MainGUI {
 	public static void main(String[] args){
+		try
+        {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
 		new MemberListGUI();		
 	}
 }
