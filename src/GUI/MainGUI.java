f1ab87fb948ca26403292b382a2a586817a11e35
Shonix <Peterfut123@gmail.com>
2015-12-14 12:46:51 +0100

    kan nu kører GUI fra main

--- src/GUI/MainGUI.java
+++ src/GUI/MainGUI.java
@@ -2,7 +2,6 @@ package GUI;
 
 public class MainGUI {
 	public static void main(String[] args){
-		//hello hello, is it me you're looking for. I can see it in your eyes, i can see it in your smile.
-		
+		new MemberListGUI();		
 	}
 }
