0b28660083aacd51447456948c498a26eeafc627
Shonix <Peterfut123@gmail.com>
2015-12-14 14:33:59 +0100

    Fixed shit

--- src/GUI/MemberListGUI.java
+++ src/GUI/MemberListGUI.java
@@ -7,6 +7,7 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
@@ -16,12 +17,15 @@ public class MemberListGUI implements ActionListener {
 	JFrame frame = new JFrame("Medlems liste");
 	JPanel panel = new JPanel();
 	JPanel panel1 = new JPanel();
-	JButton btn1 = new JButton("Tilføj");
+	JButton btnTilfoej = new JButton("Tilføj");
 	JButton btn2 = new JButton("log ud");
+	
+	private int selectedRow = 0;
 	
 	Object rowData[][] = { { "Ahmed" }, { "Niki" }, { "Dan" }, { "Peter" } };
 	Object columnNames[] = { "Medlemmer" };
 	JTable table = new JTable(rowData, columnNames);
+	private boolean isListenerActive;
 
 	public MemberListGUI() {
 
@@ -30,7 +34,8 @@ public class MemberListGUI implements ActionListener {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setLayout(new BorderLayout());
 		frame.add(panel, BorderLayout.SOUTH);
-		frame.add(btn1, BorderLayout.EAST);
+		frame.add(btnTilfoej, BorderLayout.EAST);
+		btnTilfoej.addActionListener(this);
 		panel.add(btn2);
 		jTable();
 
@@ -39,16 +44,32 @@ public class MemberListGUI implements ActionListener {
 
 	public void actionPerformed(ActionEvent e) {
 
+		if (e.getSource() == btnTilfoej){
+			String name = null;
+			selectedRow = table.getSelectedRow();
+			try {
+				name = (String) (table.getValueAt(selectedRow, 0));
+			} catch (NumberFormatException | ArrayIndexOutOfBoundsException e1) {
+				JOptionPane.showMessageDialog(frame, "Intet medlem er valgt.");
+			}
+			if(selectedRow > -1){
+				//Gør ting her.
+				System.out.println(name);
+			}
+		}
 		if (e.getSource() == btn2) {
 			frame.dispose();
 		}
-
-		else if (e.getSource() == btn1) {
-			int selectedRowIndex = table.getSelectedRow();
-			int selectedColumnIndex = table.getSelectedColumn();
-			Object selectedObject = (Object) table.getModel().getValueAt(selectedRowIndex, selectedColumnIndex);
-			System.out.println(selectedObject);
-		}
+//		else if (e.getSource() == btnTilfoej) {
+//			int selectedRowIndex = table.getSelectedRow();
+//			int selectedColumnIndex = table.getSelectedColumn();
+//			Object selectedObject = (Object) table.getModel().getValueAt(selectedRowIndex, selectedColumnIndex);
+//			if(selectedRow > -1){
+//				//Gør ting her.
+//				System.out.println(selectedObject);
+//			}
+//		}
+		
 	}
 
 	public void jTable() {
